bbcf8e9c9fc7c164f0d3c4e56f296a918debab2e
lee-soo-heon <sooheon45@uengine.org>
2020-05-13 17:52:29 +0900

    testTemplate

---
+++ public/static/templates/spring-boot/src/main/java/com/eventstorming/Application.java
@@ -0,0 +1,20 @@
+path: {{name}}/{{{options.packagePath}}}
+---
+package {{options.package}};
+import {{options.package}}.config.kafka.KafkaProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableBinding(KafkaProcessor.class)
+@EnableFeignClients
+public class Application {
+    protected static ApplicationContext applicationContext;
+    public static void main(String[] args) {
+        applicationContext = SpringApplication.run(Application.class, args);
+    }
+}
